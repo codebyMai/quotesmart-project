5e45172a28c4156458c2c1d52a751632263db66b
codebyMai <158331419+codebyMai@users.noreply.github.com>
2024-09-12 17:28:49 +0100

    fixing host issue

--- main/settings.py
+++ main/settings.py
@@ -28,9 +28,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com',]
+ALLOWED_HOSTS = ['.herokuapp.com',
+                 'localhost',
+                 '0.0.0.0',
+                 '127.0.0.1',]
 
 
 # Application definition
