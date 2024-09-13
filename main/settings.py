7480864a3ec82a92ccc43636af7e946cf5c59053
codebyMai <158331419+codebyMai@users.noreply.github.com>
2024-09-13 18:11:39 +0100

    fixing database deployment issues

--- main/settings.py
+++ main/settings.py
@@ -10,12 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-
+from pathlib import Path
 import os
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
-from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -93,6 +93,10 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost,'
+    'https://*.herokuapp.com'
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
