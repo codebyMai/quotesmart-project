0066b5e3328506c01c442ff7cd0f9f056c038ef9
codebyMai <158331419+codebyMai@users.noreply.github.com>
2024-10-04 08:19:41 +0100

    fix logo responsiveness on small screens

--- quote/forms.py
+++ quote/forms.py
@@ -1,9 +1,8 @@
 from django import forms
 from .models import Quote
 
-
+"""Add Quote form"""
 class QuoteForm(forms.ModelForm):
-  
     class Meta:
         model = Quote
         fields = ('content',
